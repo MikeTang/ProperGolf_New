5280036cb0bf18d7fdbf46073935841c18712b54
Mike Tang <mtminded@gmail.com>
2016-06-28 22:29:16 -0700

    updated some config files for server

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => DB_HOSTNAME,
-	'username' => DB_USER,
-	'password' => DB_PASS,
-	'database' => DB_NAME,
+	'hostname' => 'localhost',
+	'username' => '',
+	'password' => '',
+	'database' => '',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
