6eea9c33ab89a0baa5a96e9eb1c74d44ca3c7853
cwave1971 <cwave1971@p3nlhftpg086.shr.prod.phx3.secureserver.net>
2016-07-17 17:14:30 -0700

    Changed copy on home page and removed some files

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -44,7 +44,7 @@
   <div class="container">
       <div class="row title">
           <div class="col-md-12 t_c"><h1>Learn Golf Properly</h1></div>
-          <div class="col-md-12 t_c"><h2>Learn golf using proven methodologies in bite-sized lessons.</h2></div>
+          <div class="col-md-12 t_c"><h2>Using proven methodologies in easy to understand bite-sized lessons.</h2></div>
           
       </div>
       
@@ -67,7 +67,9 @@
         <div class="row">
             <div class="col-md-12 t_c">
                 <h2>Why ProperGolf?</h2>
-                <p>Lorem ipsum dolor sit molestiae officia. Ex temporibus quibusdam aspernatur. </p>
+                <h3>100% proven to work for players of all levels.</h3>
+                <p>The program is designed to take you step by step, providing complete instruction and drills, taliored to your existing knowledge, ability, and specific needs.  Proper check points throughout the program allows you to track your progress.  This ensures you have the proper foundation before you move on to more advanced skills and guarantees proper learning and progression to a consistant, powerful swing!
+</p>
             </div>
         </div>
          <div class="row usps">
@@ -76,22 +78,22 @@
                   <div class="usp_image"></div>
                 </div>
                 
-                <h3>USP 1</h3>
-                <p >Lorem ipsum dolor sit molestiae officia. Ex temporibus quibusdam aspernatur. </p>
+                <h3>Simple</h3>
+                <p >Complex made simple in easy to understand bite-sized lessons.</p>
             </div>
             <div class="col-md-4 t_c">
                 <div class="t_c">
                   <div class="usp_image"></div>
                 </div>
-                <h3>USP 1</h3>
-                <p>Lorem ipsum dolor sit molestiae officia. Ex temporibus quibusdam aspernatur.</p>
+                <h3>Organized</h3>
+                <p>Information is organized and taught in the proper sequence.</p>
             </div>
             <div class="col-md-4 t_c">
                 <div class="t_c">
                   <div class="usp_image"></div>
                 </div>
-                <h3>USP 1</h3>
-                <p>Lorem ipsum dolor sit molestiae officia. Ex temporibus quibusdam aspernatur.</p>
+                <h3>Proper Practice</h3>
+                <p>Learn to practice properly and see rapid progression.</p>
             </div>
         </div>
     </div>
@@ -102,12 +104,12 @@
         <div class="row">
             <div class="col-md-6">
               <br>
-              <h2>See it in action</h2>
-              <br><br>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit bibendum orci. Nunc rhoncus finibus mauris at bibendum. Ut rutrum nunc non ex interdum elementum. Curabitur rutrum enim a magna iaculis, eu elementum mi efficitur. Phasellus justo risus, porttitor vel pellentesque vitae, gravida accumsan erat.</p>
+              <h2>Frustrated?</h2>
+                <p>Golf information and instruction can be found all over YouTube and the internet. The problem is the information is incomplete, incorrect or not applicable to your needs.  It is difficult to distinguish the good from the bad.</p>
+              <h2>Introducing Propergolf</h2>
+                <p>The latest, most comprehensive, and innovative step by step golf instruction ever created.  A complete program starting with the basics and proceeding to the most advanced golf theories and techniques.  We break golf down to simple to understand components.  Each component is presented in the correct sequence and reviewed in finite detail. This ensures there is no confusion in the learning process.</p>
               <br>
-              <p>Phasellus justo risus, porttitor vel pellentesque vitae, gravida accumsan erat.</p>
-            </div>
+             </div>
             
             <div class="col-md-6">
               <div class="video_box">
@@ -132,11 +134,11 @@
             <div class="col-md-5">
                 
                 <h3>ProperGolf</h3>
-                <p >ProperGolf is not another typical program that promises to take 2 strokes off your game in 2 weeks. We have spent thousands of hours crafting this golf learning curriculum based on proved methodoligies that work for beginners and intermediate golfers.
+                <p >ProperGolf is not another typical program that promises you will be able to drive the ball 30 yards farther with this quick tip. We have spent thousands of hours crafting the ProperGolf learning curriculum based on proven methodoligies that works for golfers of all levels.
                 </p><br>
-                <p>Throughout this program, we're start with basic concepts and build upon them via convenient bite-sized videos. We will teach you how to build good habits, and also the reasoning behind them. You will have access to a personal trainer which you can ask questions and submit videos for feedback.
+                <p>Throughout the program, we start with basic concepts and build upon them via easy to follow bite-sized lessons. We teach how to build good habits, and also the logic on why those are good habits.  We provide comprehensive information so nothing is left unclear and questions and videos can be submitted for direct feedback on anything that is uncertain.
                 </p><br>
-                <p>If you're willing to put the time in to learn and practice, you will see improvements in your game in the months to come.</p>
+                <p>If you're willing to put the time in to learn and practice properly, you will see improvements in your game... it's that simple.</p>
             </div>
             
             <div class="col-md-2">
@@ -146,11 +148,11 @@
 
             <div class="col-md-5">
                 <h3>The Other Guys</h3>
-                <p>Tired of trying countless 'Improve your game in 2 weeks' programs but never actually improve?</p>
+                <p>Tired of trying countless 'Quick tips and fixes' programs that never actually improve your game?</p>
                 <br/>
                 <p>These guys captured your interest with false promises, but think about it, that's not how learning is actually done!</p> 
                 <br>
-                <p>Would you start learning piano by imitating Mozart? Of course not! You begin by learning the basics: music theory and practicing scales. It's the same in Golf, you begin by learning basic concepts and build upon it. These guys won't teach you that, but ProperGolf will.</p>
+                <p>When you start learning piano, do you look for quick tips and fixes that will allow you to play the piano like Mozart? Of course not! You begin by learning the basics: music theory and practicing scales. It's the same in Golf, you begin by learning basic concepts and build upon it. These guys won't teach you that, but ProperGolf will.</p>
             </div>
 
         </div>
